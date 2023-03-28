d44cc72b7f385689e6d214aea207446f0a6066c8
R0m5 <rlastovetskyi@gmail.com>
2023-03-28 16:17:26 +0300

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './css/styles.css';
-import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
 import { fetchCountries } from './js/fetchCountries';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector('#search-box');
@@ -14,7 +15,6 @@ function onSearch(evt) {
   evt.preventDefault();
   const trimValue = searchBox.value.trim();
   clearHtml();
-
   if (trimValue !== '') {
     fetchCountries(trimValue).then(data => {
       if (data.length > 10) {
@@ -22,7 +22,7 @@ function onSearch(evt) {
           'Too many matches found. Please enter a more specific name.'
         );
       } else if (data.length === 0) {
-        Notiflix.Notify.info('Oops, there is no country with that name');
+        Notiflix.Notify.failure('Oops, there is no country with that name');
       } else if (data.length >= 2 && data.length <= 10) {
         createListMarkup(data);
       } else if (data.length === 1) {
@@ -51,15 +51,17 @@ function createCountryList(country) {
   const card = country
     .map(
       ({ name, capital, flags, population, languages }) =>
-        `<div class = "country_item"><img class = "country_icon" src = "${
-          flags.svg
-        }" alt = "${name.official}" width = "100" height = "80"></div>
+        `<div><img class = "country_icon" src = "${flags.svg}" alt = "${
+          name.official
+        }" width = "100" height = "80"></div>
       <h1>${name.official}</h1>
-      <p class = "info">${capital}</p>
-      <p class = "info">Capital: ${capital}</p>
-      <p class = "info">Population: ${population}</p>
-      <p class = "info">Languages: ${Object.values(languages)}</p>`
+      <p>${capital}</p>
+      <p>Capital: ${capital}</p>
+      <p>Poplation: ${population}</p>
+      <p>Languages: ${Object.values(languages)}</p>`
     )
     .join(' ');
+
   countryList.innerHTML = '';
+  countryInfo.innerHTML = card;
 }
